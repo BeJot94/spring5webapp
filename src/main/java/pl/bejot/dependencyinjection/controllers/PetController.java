7f629b1af2bb18a032b7c5e633a25ae6eb70db1e
BeJot94 <bartosz.jasinski.94@gmail.com>
2021-07-05 20:36:46 +0200

    Fixing broken code for DI assignment

--- src/main/java/pl/bejot/dependencyinjection/controllers/PetController.java
+++ src/main/java/pl/bejot/dependencyinjection/controllers/PetController.java
@@ -1,10 +1,16 @@
 package pl.bejot.dependencyinjection.controllers;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
 import pl.bejot.dependencyinjection.services.PetService;
 
+@Controller
 public class PetController {
-
     private final PetService petService;
+
+    public PetController(@Qualifier("petService") PetService petService) {
+        this.petService = petService;
+    }
 
     public String whichPetIsTheBest(){
         return petService.getPetType();
--- src/main/java/pl/bejot/dependencyinjection/services/CatPetService.java
+++ src/main/java/pl/bejot/dependencyinjection/services/CatPetService.java
@@ -1,8 +1,10 @@
 package pl.bejot.dependencyinjection.services;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Service("cat")
+@Profile("cat")
+@Service("petService")
 public class CatPetService implements PetService {
     @Override
     public String getPetType() {
